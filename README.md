d815e166bfbefd59d181a360ca87e25b8eec2a6c
Simon Elder <simon.elder@transport.nsw.gov.au>
2022-06-17 15:41:10 +1000

    node 16 ... lets use npm

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 ## Setting up
 
-- `yarn install`
+- `npm install`
 - set up AWS creds in your terminal
-- `yarn sls deploy` to deploy it to AWS
+- `npm run deploy` to deploy it to AWS
 
 ## Populate some data
 
@@ -12,6 +12,6 @@
 
 ## Running
 
-- `yarn start`
+- `npm run start`
 - getting token info `curl --location --request GET 'http://localhost:3000/poc/token/36ac9124-9c17-446a-8f0c-be35eb9cc827'`
 - getting fare info `curl --location --request PATCH 'http://localhost:3000/poc/token/8785ad30-4b43-4930-988a-f0785a99a8c2/getFare'`
--- serverless.ts
+++ serverless.ts
@@ -1,73 +1,71 @@
-import type { AWS } from "@serverless/typescript";
+import type { AWS } from '@serverless/typescript';
 
 const serverlessConfiguration: AWS = {
-  service: "ong",
-  frameworkVersion: "2",
-  plugins: ["serverless-esbuild", "serverless-offline"],
+  service: 'ong',
+  frameworkVersion: '>=3.18',
+  plugins: ['serverless-esbuild', 'serverless-offline'],
   provider: {
-    name: "aws",
-    region: "ap-southeast-2",
-    stage: "poc",
-    runtime: "nodejs14.x",
+    name: 'aws',
+    region: 'ap-southeast-2',
+    stage: 'poc',
+    runtime: 'nodejs16.x',
+    deploymentMethod: 'direct',
     apiGateway: {
       minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
+      shouldStartNameWithService: true
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
-      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000'
     },
-    lambdaHashingVersion: "20201221",
     iam: {
       role: {
         statements: [
           {
-            Effect: "Allow",
+            Effect: 'Allow',
             Action: [
-              "dynamodb:GetItem",
-              "dynamodb:PutItem",
-              "dynamodb:UpdateItem",
-              "dynamodb:Query",
-              "dynamodb:DeleteItem",
+              'dynamodb:GetItem',
+              'dynamodb:PutItem',
+              'dynamodb:UpdateItem',
+              'dynamodb:Query',
+              'dynamodb:DeleteItem'
             ],
             Resource: [
               {
-                "Fn::Sub":
-                  "arn:aws:dynamodb:${AWS::Region}:${AWS::AccountId}:table/ong-tokens-${sls:stage}",
+                'Fn::Sub': 'arn:aws:dynamodb:${AWS::Region}:${AWS::AccountId}:table/ong-tokens-${sls:stage}'
               },
               {
-                "Fn::Sub":
-                  "arn:aws:dynamodb:${AWS::Region}:${AWS::AccountId}:table/ong-taps-${sls:stage}",
-              },
-            ],
-          },
-        ],
-      },
-    },
+                'Fn::Sub': 'arn:aws:dynamodb:${AWS::Region}:${AWS::AccountId}:table/ong-taps-${sls:stage}'
+              }
+            ]
+          }
+        ]
+      }
+    }
   },
   functions: {
     getToken: {
-      handler: "./src/functions/getToken.handler",
+      handler: './src/functions/getToken.handler',
       events: [
         {
           http: {
-            method: "get",
-            path: "/token/{tokenId}",
-          },
-        },
-      ],
+            method: 'get',
+            path: '/token/{tokenId}'
+          }
+        }
+      ]
     },
     getFare: {
-      handler: "./src/functions/getFare.handler",
+      handler: './src/functions/getFare.handler',
       events: [
         {
           http: {
-            method: "patch",
-            path: "/token/{tokenId}/getFare",
-          },
-        },
-      ],
-    },
+            method: 'patch',
+            path: '/token/{tokenId}/getFare'
+          }
+        }
+      ]
+    }
   },
   package: { individually: true },
   custom: {
@@ -75,75 +73,75 @@ const serverlessConfiguration: AWS = {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ["aws-sdk"],
-      target: "node14",
-      define: { "require.resolve": undefined },
-      platform: "node",
-      concurrency: 10,
-    },
+      exclude: ['aws-sdk'],
+      target: 'node16',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10
+    }
   },
   resources: {
     Resources: {
       TokensTable: {
-        Type: "AWS::DynamoDB::Table",
+        Type: 'AWS::DynamoDB::Table',
         Properties: {
-          TableName: "ong-tokens-${sls:stage}",
+          TableName: 'ong-tokens-${sls:stage}',
           AttributeDefinitions: [
             {
-              AttributeName: "tokenId",
-              AttributeType: "S",
-            },
+              AttributeName: 'tokenId',
+              AttributeType: 'S'
+            }
           ],
           KeySchema: [
             {
-              AttributeName: "tokenId",
-              KeyType: "HASH",
-            },
+              AttributeName: 'tokenId',
+              KeyType: 'HASH'
+            }
           ],
-          BillingMode: "PAY_PER_REQUEST",
-        },
+          BillingMode: 'PAY_PER_REQUEST'
+        }
       },
       TapsTable: {
-        Type: "AWS::DynamoDB::Table",
+        Type: 'AWS::DynamoDB::Table',
         Properties: {
-          TableName: "ong-taps-${sls:stage}",
+          TableName: 'ong-taps-${sls:stage}',
           AttributeDefinitions: [
             {
-              AttributeName: "tokenId",
-              AttributeType: "S",
+              AttributeName: 'tokenId',
+              AttributeType: 'S'
             },
             {
-              AttributeName: "tapId",
-              AttributeType: "S",
+              AttributeName: 'tapId',
+              AttributeType: 'S'
             },
             {
-              AttributeName: "tapTime",
-              AttributeType: "N",
-            },
+              AttributeName: 'tapTime',
+              AttributeType: 'N'
+            }
           ],
           KeySchema: [
             {
-              AttributeName: "tapId",
-              KeyType: "HASH",
-            },
+              AttributeName: 'tapId',
+              KeyType: 'HASH'
+            }
           ],
           GlobalSecondaryIndexes: [
             {
-              IndexName: "tripsByToken",
+              IndexName: 'tripsByToken',
               KeySchema: [
-                { AttributeName: "tokenId", KeyType: "HASH" },
-                { AttributeName: "tapTime", KeyType: "RANGE" },
+                { AttributeName: 'tokenId', KeyType: 'HASH' },
+                { AttributeName: 'tapTime', KeyType: 'RANGE' }
               ],
               Projection: {
-                ProjectionType: "ALL",
-              },
-            },
+                ProjectionType: 'ALL'
+              }
+            }
           ],
-          BillingMode: "PAY_PER_REQUEST",
-        },
-      },
-    },
-  },
+          BillingMode: 'PAY_PER_REQUEST'
+        }
+      }
+    }
+  }
 };
 
 module.exports = serverlessConfiguration;
